f46a3faa2d93fdac2d7a094aa9a6813f95cec013
rohithkumar0369 <rohithkumar0369@gmail.com>
2024-09-26 20:30:51 +0530

    feat: additional api added

---
+++ src/controller/user/dao.ts
@@ -0,0 +1,29 @@
+import { registerDetails } from "../../model/user";
+import { UserModel } from "../../types";
+
+export const getuserDao = async (wallet_address: string) => {
+  const user = await registerDetails.findOne({
+    wallet_address,
+  });
+  return user as UserModel;
+};
+
+export const getAlluserDao = async () => {
+  const user = await registerDetails.find({});
+  return user as UserModel[];
+};
+
+export const postUserDao = async (recievedData: UserModel) => {
+  const user = new registerDetails(recievedData);
+  const res = await user.save();
+  return res;
+};
+
+export const updateUserDao = async (recievedData: UserModel) => {
+  const updatedUser = await registerDetails.findOneAndUpdate(
+    { wallet_address: recievedData.wallet_address },
+    recievedData,
+    { new: true }
+  );
+  return updatedUser;
+};
--- src/model/user.ts
+++ src/model/user.ts
@@ -1,38 +1,106 @@
 import mongoose from "mongoose";
+import { UserModel } from "../types";
 
 const schema = mongoose.Schema;
 
-const registerSchema = new schema({
+const registerSchema = new schema<UserModel>({
   wallet_address: {
     type: String,
+    required: true,
+    unique: true,
   },
   twitter_username: {
     type: String,
+    default: "",
   },
-  is_Verified: {
-    type: Boolean,
+  is_twitter_verified: {
+    type: String,
+    default: "false",
+  },
+  discord_username: {
+    type: String,
+    default: "",
+  },
+  is_discord_verified: {
+    type: String,
+    default: "false",
+  },
+  telegram_username: {
+    type: String,
+    default: "",
+  },
+  is_telegram_verified: {
+    type: String,
+    default: "false",
   },
   wallets_last_sequence_no: {
     type: Number,
+    default: 0,
   },
   wallets_last_version_no: {
     type: Number,
+    default: 0,
   },
   wallet_score: {
     type: Number,
+    default: 0,
   },
   kyc_points: {
     type: Number,
+    default: 0,
   },
   biometric_points: {
     type: Number,
+    default: 0,
   },
   social_points: {
     type: Number,
+    default: 0,
   },
   on_chain_points: {
     type: Number,
+    default: 0,
   },
+});
+
+// Pre-save middleware to calculate sum
+registerSchema.pre("save", function (next) {
+  this.wallet_score =
+    this.kyc_points +
+    this.on_chain_points +
+    this.biometric_points +
+    this.social_points; // Recalculating sum before saving
+  next();
+});
+
+// Pre-hook for update operations
+registerSchema.pre("findOneAndUpdate", async function (next) {
+  const update = this.getUpdate() as UserModel;
+
+  // If any of the relevant fields are being updated
+  if (
+    update.kyc_points !== undefined ||
+    update.on_chain_points !== undefined ||
+    update.biometric_points !== undefined ||
+    update.social_points !== undefined
+  ) {
+    // Retrieve the current document from the database
+    const docToUpdate = await this.model.findOne(this.getFilter());
+
+    // Recalculate the wallet score if the document exists
+    if (docToUpdate) {
+      const newWalletScore =
+        (update.kyc_points ?? docToUpdate.kyc_points) +
+        (update.on_chain_points ?? docToUpdate.on_chain_points) +
+        (update.biometric_points ?? docToUpdate.biometric_points) +
+        (update.social_points ?? docToUpdate.social_points);
+
+      // Set the recalculated wallet score
+      this.setUpdate({ ...update, wallet_score: newWalletScore });
+    }
+  }
+
+  next();
 });
 
 export const registerDetails = mongoose.model("user", registerSchema, "user");
