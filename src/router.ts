5e3897369f187dbfb323508fe30386f7da0a6e3a
gunaseelan <gunaon2509@gmail.com>
2024-10-14 01:00:33 +0530

    feat: add mobile

--- src/router.ts
+++ src/router.ts
@@ -4,11 +4,13 @@ import verifyTwiter from "./controller/twitter";
 import userController from "./controller/user";
 import signController from "./controller/signController";
 import verifyTelagram from "./controller/telegram";
+import verifyMobile from "./controller/mobile";
 
 export default async function router(fastify: FastifyInstance) {
   fastify.register(verificationController, { prefix: "/verify" });
   fastify.register(verifyTwiter, { prefix: "/twitter" });
   fastify.register(verifyTelagram, { prefix: "/telegram" });
+  fastify.register(verifyMobile, { prefix: "/mobile" });
   fastify.register(userController, { prefix: "/user" });
   fastify.register(signController, { prefix: "/sign" });
 }
